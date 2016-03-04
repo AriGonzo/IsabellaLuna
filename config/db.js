b2fec60ad0067ecfc680c319f83daebc7a222eb9
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2016-03-03 21:25:30 -0500

    config of db

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 // config/db.js
 module.exports = {
-  url : 'mongodb://localhost:27017/isabelly'
+  //url : 'mongodb://localhost:27017/isabelly'
+  url: 'mongodb://AriGonzo:182Blink@ds019648.mlab.com:19648/isabellaluna'
 }
