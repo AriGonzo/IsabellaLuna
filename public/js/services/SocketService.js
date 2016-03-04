58c3b2c93a200c07477fc29e568baf3f9d611a67
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2016-03-03 21:11:04 -0500

    change of server base url socket.io

--- public/js/services/SocketService.js
+++ public/js/services/SocketService.js
@@ -1,6 +1,6 @@
 angular.module('SocketService', ['btford.socket-io'])
   .factory('socket', function (socketFactory) {
-    var serverBaseUrl = 'http://localhost:8080';
+    var serverBaseUrl = 'http://52.87.216.253:9999';
     var myIoSocket = io.connect(serverBaseUrl);
 
     var socket = socketFactory({
