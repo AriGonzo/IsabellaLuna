152eab02ea4cad84a9ee81291bae0d14a71fda40
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2016-03-03 20:54:07 -0500

    change port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var sassMiddleware = require('node-sass-middleware');
 var db = require('./config/db');
 
 // set our port
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 
 // connect to our mongoDB database
 // (uncomment after you enter in your own credentials in config/db.js)
